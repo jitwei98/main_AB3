3f75a6c7d55377b2abf71288af2c5d8a25b036fc
jitwei98 <limjitwei@gmail.com>
2018-09-27 13:06:51 +0800

    getPrintableString takes in argument of boolean hidePrivate

--- src/seedu/addressbook/data/person/Printable.java
+++ src/seedu/addressbook/data/person/Printable.java
@@ -4,6 +4,6 @@ package seedu.addressbook.data.person;
  * A printable interface for a Person.
  */
 public interface Printable {
-
-    String getPrintableString(Printable... printables);
+    String detailIsPrivate = "(private) ";
+    String getPrintableString(boolean hidePrivate);
 }
