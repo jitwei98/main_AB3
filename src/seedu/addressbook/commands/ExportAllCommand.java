49cea58fd726725b7c904313305addd56f06c04e
jitwei98 <limjitwei@gmail.com>
2018-10-01 15:25:54 +0800

    Modify ExportAllCommand.java

--- src/seedu/addressbook/commands/ExportAllCommand.java
+++ src/seedu/addressbook/commands/ExportAllCommand.java
@@ -4,13 +4,16 @@ package seedu.addressbook.commands;
  * Export all persons from the address book into a csv/vcf file.
  */
 public class ExportAllCommand extends Command {
+
     public static final String COMMAND_WORD = "exportall";
 
-    public ExportAllCommand() {
-    }
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "ExportAll command not implemented yet";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Export all the contacts in the addressbook "
+            + "Parameters: FILETYPE (must be either \"csv\" or \"vcf\") ";
 
     @Override
     public ExportAllCommand execute() throws Exception {
-        throw new Exception();
+        throw new Exception(MESSAGE_NOT_IMPLEMENTED_YET);
     }
 }
